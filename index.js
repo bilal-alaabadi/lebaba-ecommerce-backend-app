fc6878a388d04b91d9f8f61b76a13701cc56f591
bial <mdbilalnizwa@gmail.com>
2025-01-17 18:55:36 +0400

    url mak updafft

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ app.use(
     })
 );
 
+// دعم طلبات OPTIONS (Preflight Requests)
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://lebab-frontend-final.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.send();
+})
+
 // رفع الصور
 const uploadImage = require("./src/utils/uploadImage");
 

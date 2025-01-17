9760961db1b60b7dda49d4b0adcc8bcf86bb7987
bial <mdbilalnizwa@gmail.com>
2025-01-17 17:31:53 +0400

    url updat

--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -51,8 +51,8 @@ router.post("/create-checkout-session", async (req, res) => {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: "payment",
-            success_url: "http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: "http://localhost:5173/cancel",
+            success_url: "https://lebaba-ecommerce-backend-app-snowy.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url: "https://lebaba-ecommerce-backend-app-snowy.vercel.app/cancel",
             metadata: {
                 province,
                 wilayat,

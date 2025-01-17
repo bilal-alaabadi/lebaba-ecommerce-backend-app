d13c21653d99779f0bb39694cb6e09c3c714896b
bial <mdbilalnizwa@gmail.com>
2025-01-17 18:42:31 +0400

    url updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://lebab-frontend-final.vercel.app",//مال الفرونت اند
         credentials: true,
     })
 );
